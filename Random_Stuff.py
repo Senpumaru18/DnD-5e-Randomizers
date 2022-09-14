d93e2a7e17444e57628ff4122c7d8732cd512605
Blake-Johnson18 <jc8822wf@go.minnstate.edu>
2022-09-14 11:56:00 -0500

    random stuff

--- Random_Stuff.py
+++ Random_Stuff.py
@@ -5,7 +5,13 @@ alphabet = asc + "!@#$%^&*"
 def CeaserCipher():
     message = str(input("Enter your message: "))
     shift = int(input("Enter the amount to shift: "))
-    shifted = []
-    for x in range(alphabet):
-        
+    shifted = alphabet[shift:] + alphabet[:shift]
+    for x in range(len(alphabet)):
+        coded = ""
+        letter = message[x]
+        newLetter = shifted[letter]
+    print(shifted)
+    print(letter)
 
+
+CeaserCipher()
