6771c4aea5106ae9237352375400f0a370523fd7
Blake-Johnson18 <jc8822wf@go.minnstate.edu>
2022-09-15 10:09:25 -0500

    Not D&D

--- Random_Stuff.py
+++ Random_Stuff.py
@@ -1,11 +1,10 @@
 from string import ascii_lowercase as asc
 from string import ascii_uppercase as ASC
 
-alphabet = asc + "!@#$%" + ASC + "^&* "
+alphabet = asc + '{} !"@#$%*,&.^012345678?9' + ASC      # Cengage 4.1
+# alphabet = asc + "{}@ !%#$*&',^.9012345678?" + ASC    # Cengage 4.2
 
-def CeaserCipher():
-    message = str(input("Enter your message: "))
-    shift = int(input("Enter the amount to shift: "))
+def CeaserCipher(message = str(input("Enter your message: ")),shift = int(input("Enter the amount to shift: "))):
     shifted = alphabet[shift:] + alphabet[:shift]
     coded = ""
     for x in range(len(message)):
@@ -14,7 +13,15 @@ def CeaserCipher():
         newLetter = shifted[letteridx]
         coded += newLetter
     print(coded)
-
-
+def CeaserDecoder(message = str(input("Enter your message: ")),shift = int(input("Enter the amount to shift: "))):
+    shifted = alphabet[shift:] + alphabet[:shift]
+    coded = ""
+    for x in range(len(message)):
+        letter = message[x]
+        letteridx = shifted.index(letter)
+        newLetter = alphabet[letteridx]
+        coded += newLetter
+    print(coded)
 
 CeaserCipher()
+CeaserDecoder()
