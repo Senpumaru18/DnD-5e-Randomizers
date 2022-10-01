71b88e44199bde9eab4e41056c01f78f95f65b2f
Senpumaru18 <stormtiger18@gmail.com>
2022-10-01 17:40:08 -0500

    eidts

--- Weapon-Info.py
+++ Weapon-Info.py
@@ -1,18 +1,19 @@
+from itertools import chain
 from random import randint
 # import Character # Needed for some weapon properties
 def workspace():
 # Space for all the weapons
-    club = meleeWeapon('Club', 10, [1,4], 'bludgeoning', 2, ['light'], 'Simple')
-    dagger = meleeWeapon('Dagger', 200, [1,4], 'piercing', 1, ['finesse', 'light', 'thrown'], 'Simple', [20,60])
-    greatClub = meleeWeapon('Great Club', 20, [1,8], 'bludgeoning', 10, ['two_handed'], 'Simple')
-    handaxe = meleeWeapon('Handaxe', 500, [1,6], 'slashing', 2, ['light','thrown'], 'Simple', [20,60])
-    javelin = meleeWeapon('Javelijn', 50, [1,6], 'piercing', 2, ['thrown'], 'Simple', [30,120])
-    lightHammer = meleeWeapon('Light Hammer', 200, [1,4], 'bludgeoning', 2, ['light', 'thrown'], 'Simple', [20,60])
+    club = meleeWeapon('Club', 10, [1,4], 'bludgeoning', 2, [light()], 'Simple')
+    dagger = meleeWeapon('Dagger', 200, [1,4], 'piercing', 1, [finesse(), light(), thrown([20,60])], 'Simple', [20,60])
+    greatClub = meleeWeapon('Great Club', 20, [1,8], 'bludgeoning', 10, [two_handed()], 'Simple')
+    handaxe = meleeWeapon('Handaxe', 500, [1,6], 'slashing', 2, [light(),thrown([20,60])], 'Simple', [20,60])
+    javelin = meleeWeapon('Javelijn', 50, [1,6], 'piercing', 2, [thrown([30,120])], 'Simple', [30,120])
+    lightHammer = meleeWeapon('Light Hammer', 200, [1,4], 'bludgeoning', 2, [light(), thrown([20,60])], 'Simple', [20,60])
     mace = meleeWeapon('Mace', 500, [1,6], 'bludgeoning', 4, [], 'Simple')
-    quarterstaff = meleeWeapon('Quarterstaff', 20, [1,6], 'bludgeoning', 4, ['Versatile 1d8'], 'Simple')
-    sickle = meleeWeapon('Sickle', 100, [1,4], 'slashing', 2, ['light'], 'Simple')
-    spear = meleeWeapon('Spear', 100, [1,6], 'piercing', 3, ['thrown', 'versatile 1d8'], 'Simple', [20/60])
-    lightCrossbow = rangedWeapon('Crossbow, Light', 2500, [1,8], 'piercing', 5, ['Ammuntion', 'loading', 'two-handed'], 'Simple', [80,320])
+    quarterstaff = meleeWeapon('Quarterstaff', 20, [1,6], 'bludgeoning', 4, [versatile([1,8])], 'Simple')
+    sickle = meleeWeapon('Sickle', 100, [1,4], 'slashing', 2, [light()], 'Simple')
+    spear = meleeWeapon('Spear', 100, [1,6], 'piercing', 3, [thrown([20,60]), versatile([1,8])], 'Simple', [20,60])
+    lightCrossbow = rangedWeapon('Crossbow, Light', 2500, [1,8], 'piercing', 5, [ammunition('bolt'), loading(), two_handed()], 'Simple', [80,320])
     # dart = rangedWeapon('Dart')
     # shortbow = rangedWeapon('Shortbow')
     # sling = rangedWeapon('Sling')
@@ -47,43 +48,42 @@ def workspace():
 
 
 # Classes for various things
-class property():
-    pass
-class thrown(property):
-    def __init__(self, _range):
-        self.range = _range  
-class ammunition(property):
+class Player:
+    def __init__(self,STR,DEX,CON,INT,WIS,CHA):
+        self.str = STR
+        self.dex = DEX
+        self.con = CON
+        self.int = INT
+        self.wis = WIS
+        self.cha = CHA       
+        
+class property_:
+    pass 
+class ammunition(property_):
     def __init__(self, ammoType):
         self.ammoType = ammoType
-class finesse(property):
+class finesse(property_):
     def STRorDEX(choice, player):
         if choice == "str":
-            return player.STR_modifier
+            return player.str
         elif choice == "dex":
-            return player.DEX_modifier
-class heavy(property):
+            return player.dex
+class heavy(property_):
     pass
-class light(property):
+class light(property_):
     pass
-class loading(property):
+class loading(property_):
     pass
-class reach(property):
+class reach(property_):
     pass
-class thrown(property):
+class thrown(property_):
     def __init__(self, _range):
         self.range = _range
-class two_handed(property):
+class two_handed(property_):
     pass
-class versatile(property):
-    def rollVersDmg(self, crit=0):
-        damage = 0
-        if crit == 0:
-            for i in range(self.damage[0]):
-                damage += randint(1, self.damage[1])
-        else:
-            for i in range(self.damage[0]*2):
-                damage += randint(1, self.damage[1])
-        return damage
+class versatile(property_):
+    def __init__(self, damage) -> None:
+        self.damage = damage
 
 class weapon:
     def __init__(self, name, cost, damage, dmgType, weight, properties, weaponGrade):
@@ -176,8 +176,9 @@ class meleeWeapon(weapon):
             self.range = _range
         else:
             pass
-    def getRange(self):
-        return f"{self.range[0]}/{self.range[1]}"
+        for prop in self.properties:
+            prop
+    # def 
 
 
 if __name__ == "__main__":
